606497bbdfa1e7bcb8ac16b6e621acd1bef7e0af
CalinRosca <57733569+CalinRosca@users.noreply.github.com>
2020-05-01 20:14:26 +0300

    Add files via upload

---
+++ Rosca_Calin_9A.java
@@ -0,0 +1,89 @@
+
+
+import java.util.Scanner;
+public class Sample{
+
+    public static void main(String args[]){
+Scanner s=new Scanner (System.in);
+int n=s.nextInt();
+int i=2;
+do{
+    System.out.println(i);
+    i+=2;
+    }
+    while(i<=n);
+
+    
+}
+}
+
+
+
+
+import java.util.Scanner;
+public class Sample{
+
+    public static void main(String args[]){
+Scanner s=new Scanner (System.in);
+int n=s.nextInt();
+double i=1;
+double z=0;
+
+do{
+    int w=(int)Math.pow(i,2.0);
+    z=z+w;
+    i++;
+    }
+    while(i<=n);
+
+System.out.println(z);
+    
+}
+}
+
+
+
+import java.util.Scanner;
+public class Sample{
+
+    public static void main(String args[]){
+int i=1;
+while(i<=5){
+    int c=1;
+    while(c<=i){
+     System.out.printf("5%d",c);
+     c++;
+        
+        
+    }
+    i++;
+    System.out.println("\n");
+    
+}
+
+    
+}
+}
+
+    
+
+  
+}
+}
+
+    
+
+    
+        
+    }
+    i++;
+    System.out.println("\n");
+    
+}
+
+    
+}
+}
+
+    }
+}
